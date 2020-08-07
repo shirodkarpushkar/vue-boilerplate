23ce71f42e0c1a58c1d1303e17133d57749926ca
shirodkarpushkar <pushkars@winjit.com>
2020-08-07 15:02:27 +0530

    deployment changed to gh-pages

--- src/router/views/home.vue
+++ src/router/views/home.vue
@@ -68,7 +68,7 @@
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
         <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
+          <h1 class="display-3">Hello, world! v1.0</h1>
           <p
             >This is a template for a simple marketing or informational website.
             It includes a large callout called a jumbotron and three supporting
