e30f084fc1960b0e05743e02f8951b1400d32642
Vipul Wairagade <sonu.wairagade@gmail.com>
2020-08-07 17:08:44 +0530

    Using 'hash' Route Mode

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = new VueRouter({
   // This may require some server configuration in production:
   // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
   base: process.env.BASE_URL,
-  mode: 'history',
+  mode: 'hash',
   // Simulate native-like scroll behavior when navigating to a new
   // route and using back/forward buttons.
   scrollBehavior(to, from, savedPosition) {
